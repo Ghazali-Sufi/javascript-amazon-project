e714e2e9837c68355bfb55baf6a69954683f31f5
Ghazali-Sufi <mohagazaali@gmail.com>
2024-08-01 14:27:31 +0300

    Delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,15 +1,15 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
-let cartSummaryHTML = '';
+let cartSummaryHTML = "";
 
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
   let matchingProduct;
   products.forEach((product) => {
     if (product.id === productId) {
-      matchingProduct = product; 
+      matchingProduct = product;
     }
   });
 
@@ -39,7 +39,9 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary" data-product-id="${
+                    matchingProduct.id
+                  }">
                     Delete
                   </span>
                 </div>
@@ -96,3 +98,9 @@ cart.forEach((cartItem) => {
 
 document.querySelector(".order-summary").innerHTML = cartSummaryHTML;
 
+document.querySelectorAll(".delete-quantity-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+  });
+});
