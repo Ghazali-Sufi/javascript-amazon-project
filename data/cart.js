dd049d43470d1c802608646886a50cc329512d3b
Ghazali-Sufi <mohagazaali@gmail.com>
2024-08-09 15:30:46 +0300

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -4,7 +4,7 @@ if (!cart) {
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
-      deliveryOptionId: "1",
+      deliveryOptionId: "2",
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
@@ -37,7 +37,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: quantity,
-      deliveryOptionId: '1'
+      deliveryOptionId: "1",
     });
   }
   saveToStorage();
@@ -66,10 +66,23 @@ export function calculateCartQuantity() {
 export function updateQuantity(productId, newQuantity) {
   let matchingItem;
   cart.forEach((cartItem) => {
-    if(productId === cartItem.productId) {
+    if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
-  })
-    matchingItem.quantity = newQuantity;
-    saveToStorage();
+  });
+  matchingItem.quantity = newQuantity;
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.id = deliveryOptionId;
+
+  saveToStorage();
 }
