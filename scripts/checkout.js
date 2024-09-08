a5a14366f3d13b36fd3392fde6e77dfed23d04c8
Ghazali-Sufi <mohagazaali@gmail.com>
2024-09-08 18:41:19 +0300

    Convert loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,28 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js'
 // import '../data/car.js';
 // import '../data/backend-practice.js'
 
+Promise.all([
+    
+ loadProductsFetch(),
+
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+/*
 Promise.all([
   new Promise((resolve) => {
     // we need to send a request, and then use a callback to wait for a response, and then run the rest of our code: Thats how we use a backend in our project
@@ -27,6 +43,8 @@ Promise.all([
       renderOrderSummary();
       renderPaymentSummary();
 });
+*/
+
 
 /*
 new Promise((resolve) => {
